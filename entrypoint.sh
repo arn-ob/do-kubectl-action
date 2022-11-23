c4bcc785934a38f4c9b3380f24af9269bd70d63e
Arnob <arnobxtreme@gmail.com>
2022-11-23 17:48:18 +0600

    do token add & docker composer add

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,15 @@
 echo "DigitalOcean version"
 
 doctl version
+
+echo "DO Token Init"
+
+doctl auth init -t "Access Tokens"
+
+echo "DigitalOcean add the cluster kubeconfig"
+
+doctl kubernetes cluster kubeconfig save <Cluster-Certificate>
+
+echo "Kubectl pod list"
+
+kubectl get pods
