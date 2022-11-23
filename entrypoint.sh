a36e553fc223bb3595ad5e87c53bff76a8f1e8ce
Arnob <arnobxtreme@gmail.com>
2022-11-23 23:05:13 +0600

    version updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ doctl auth init -t "${INPUT_DO_ACCESS_TOKEN}"
 
 echo "DigitalOcean add the cluster kubeconfig"
 
-doctl kubernetes cluster kubeconfig save "${INPUT_DO_CLUSTER_CERTIFICATE}""
+doctl kubernetes cluster kubeconfig save "${INPUT_DO_CLUSTER_CERTIFICATE}"
 
 echo "Kubectl pod list"
 
