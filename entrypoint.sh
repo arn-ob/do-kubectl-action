a690b65860d52aae184270b5058517e398908a79
Arnob <arnobxtreme@gmail.com>
2022-11-23 22:58:36 +0600

    link with github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-echo "Hello - ${INPUT_DO_ACCESS_TOKEN}"
-
 echo "DigitalOcean version"
 
 doctl version
 
 echo "DO Token Init"
 
-doctl auth init -t "Access Tokens"
+doctl auth init -t "${INPUT_DO_ACCESS_TOKEN}"
 
 echo "DigitalOcean add the cluster kubeconfig"
 
-# doctl kubernetes cluster kubeconfig save <Cluster-Certificate>
+doctl kubernetes cluster kubeconfig save "${INPUT_DO_CLUSTER_CERTIFICATE}""
 
 echo "Kubectl pod list"
 
