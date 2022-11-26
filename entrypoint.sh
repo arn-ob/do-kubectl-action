c9a918221f061064a1bb6989aeb1c9b045397d7f
Arnob <arnobxtreme@gmail.com>
2022-11-26 18:39:31 +0600

    export add

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+if [ -n "${INPUT_DO_ACCESS_TOKEN:-}" ]; then
+    export DO_ACCESS_TOKEN="${INPUT_DO_ACCESS_TOKEN}"
+fi
+
 echo "DigitalOcean version"
 
 doctl version
 
 echo "DO Token Init"
 
-doctl auth init -t "${INPUT_DO_ACCESS_TOKEN}"
+doctl auth init -t "${DO_ACCESS_TOKEN}"
 
 echo "DigitalOcean add the cluster kubeconfig"
 
