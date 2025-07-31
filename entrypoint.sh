cfd651f4831fae8dfcd26019c434484a2a17a269
Arnob <arnobxtreme@gmail.com>
2025-07-31 09:10:41 +0600

    adding deployment type

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,4 +20,4 @@ doctl kubernetes cluster kubeconfig save ${INPUT_DO_CLUSTER_CERTIFICATE}
 
 echo "Kubectl deployment"
 
-kubectl set image -n ${INPUT_DO_NAMESPACE} deployment/${INPUT_DO_DEPLOYMENT_NAME} ${INPUT_DO_CONTAINER_NAME}=${INPUT_DO_IMAGE_TAG}
+kubectl set image -n ${INPUT_DO_NAMESPACE} ${INPUT_DO_DEPLOYMENT_TYPE}/${INPUT_DO_DEPLOYMENT_NAME} ${INPUT_DO_CONTAINER_NAME}=${INPUT_DO_IMAGE_TAG}
